5e685567d33e840d4c6af6e758df6f800339fc08
Mohammed Hammad <mohamedhammad923@gmail.com>
2023-12-04 13:27:40 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+
+console.log(fizzBazz('mo'));
+
+function fizzBazz(input){
+    if (typeof input !== 'number') 
+        return NaN;
+    else if (input%5 ===0 && input%3 ===0) 
+        return 'fizzBuzz';
+    else if(input%3 ===0) 
+        return 'fizz';
+    else if (input%5 ===0) 
+        return 'Buzz';
+    else 
+        return input;
+}

f8777b334ba62cbf93205729c47f39f8514324b7
Mohammed Hammad <mohamedhammad923@gmail.com>
2023-12-04 14:30:00 +0200

    adding two problems

--- index.js
+++ index.js
@@ -13,3 +13,32 @@ function fizzBazz(input){
     else 
         return input;
 }
+// ----------------------------
+//Demerit points
+let mySpeed=70;
+console.log(checkSpeed(mySpeed));
+
+function checkSpeed(speed){
+    let speedLimit = 70;
+    let KmPerPoint =5;
+    let points = Math.floor((speed-speedLimit)/KmPerPoint);
+    if(points>=12)
+        return 'Licensece Suspended';
+    else if (points<=0)
+        return 'your are OK';
+    return points + ' points';
+}
+
+//------------------------------
+//Even and odd numbers
+let myNumber =13;
+console.log(showNumbers(myNumber))
+
+function showNumbers(myNumber){
+    for(let i=0 ;i<=myNumber ;i++){
+        const message=(i%2 === 0)?'Even':'ODD';
+        console.log(i,message)
+    }
+}
+
+ 
